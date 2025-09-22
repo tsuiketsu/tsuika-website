07ed74781b42ad32a1926f134f142951dd9bf05b
ImRayy <souradeep_roy@pm.me>
2025-09-23 01:07:05 +0530

    chore: update highlights list

--- src/components/sections/highlights/list.ts
+++ src/components/sections/highlights/list.ts
@@ -2,6 +2,24 @@ import type { Highlight } from './types';
 
 export const highlights = [
   {
+    title: 'Collaborative',
+    description:
+      'Invite other users by their email or username to collaborate on projects and achieve great results together.',
+    icon: 'tabler--users-group'
+  },
+  {
+    title: 'Publish',
+    description:
+      'Share folders with anyone, either publicly or secured with a password for controlled access.',
+    icon: 'tabler--world-share'
+  },
+  {
+    title: 'Encrypted Folders',
+    description:
+      'Protect your folders with end-to-end encryption using the XChaCha20-Poly1305 algorithm for both security and efficiency.',
+    icon: 'tabler--shield'
+  },
+  {
     title: 'Open Source',
     description:
       'This project is open source under the AGPL-3.0 license, ensuring it remains freely accessible and cannot become closed source in the future.',
@@ -32,9 +50,16 @@ export const highlights = [
     icon: 'tabler--checkbox'
   },
   {
-    title: 'Powerful Search',
+    title: 'PWA',
     description:
-      'Effortlessly search through all your bookmarks using full-text search.',
-    icon: 'tabler--list-search'
+      'Experience our app as a Progressive Web App, offering lightning-fast performance. Install it on any device for a native-like experience with seamless updates',
+    icon: 'tabler--app-window'
+  },
+
+  {
+    title: 'Markdown Editor',
+    description:
+      'Write bookmark descriptions or full articles with feature-rich WSAM Markdown editor, featuring real-time previews, syntax highlighting for seamless, polished content creation.',
+    icon: 'tabler--markdown'
   }
 ] satisfies Highlight[];
