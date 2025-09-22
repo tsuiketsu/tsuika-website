68ea1554e8b02acfead0c3b98031d8129efba989
ImRayy <souradeep_roy@pm.me>
2025-09-23 01:22:29 +0530

    style(header): update logo format and font

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -7,9 +7,9 @@
 
 <div class="bg-background sticky top-0 z-20 flex w-full justify-center text-sm">
   <div class="inline-flex w-full max-w-6xl items-center gap-2 px-4 py-2">
-    <Button variant="link" target="_self" class="text-lg font-bold" href="/">
-      🔖 Tsuika
-    </Button>
+    <a target="_self" class="font-yellowtail text-3xl font-bold" href="/"
+      >Tsuika
+    </a>
     <div class="ml-auto">
       <ThemeToggle />
     </div>
