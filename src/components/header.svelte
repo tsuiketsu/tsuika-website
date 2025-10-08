ef465f2589aaf88da4c2271ea5b629a242a6146c
ImRayy <souradeep_roy@pm.me>
2025-10-09 01:12:03 +0530

    style(header): increase y axis padding

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="bg-background sticky top-0 z-20 flex w-full justify-center text-sm">
-  <div class="inline-flex w-full max-w-6xl items-center gap-2 px-4 py-2">
+  <div class="inline-flex w-full max-w-6xl items-center gap-2 px-4 py-4">
     <a target="_self" class="font-yellowtail text-3xl font-bold" href="/"
       >Tsuika
     </a>
